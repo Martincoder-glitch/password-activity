ab1ae514816c11551d7f99f464147b5ded65a33b
Martincoder-glitch <Martinsasuke20241@outlook.com>
2025-01-26 17:50:50 +0000

    Add files via upload

--- fruit ninja.py
+++ fruit ninja.py
@@ -5,14 +5,20 @@ WIDTH=500
 goku=Actor("goku")
 goku.x=200
 goku.y=100
-images=["apple","banana","mango","pineapple","watermelon"]
+images=["apple","banana","mango","pineapple","watermelon","bomb"]
 fruits=[]
+score=0
+game=True
 def draw():
     
     screen.blit("background",(0,0))
+    screen.draw.text("score:"+str(score),color="black",topleft=(10,10)) 
     goku.draw()
     for fruit in fruits:
         fruit.draw()
+    if game == False:
+        screen.fill("red")
+        screen.draw.text("gameover",topleft=(10,10),color="blue")
 def on_mouse_move(pos):
     if goku.collidepoint(pos):
         goku.pos=pos
@@ -23,8 +29,18 @@ def make_fruit():
     fruit.x=random.randint(50,WIDTH-50)
     fruit.y=random.randint(50,HEIGHT-50)
     fruits.append(fruit)
-    
-clock.schedule_interval(make_fruit,15)
+def update():
+    global score,game,fruits
+    for fruit in fruits: 
+        fruit.y +=random.randint(1,5)
+        if goku.colliderect(fruit):
+            if fruit.image =="bomb":
+                game=False
+            else:
+                fruits.remove(fruit)
+                score +=1  
+                fruits=[]  
+clock.schedule_interval(make_fruit,1)
 
 
 
