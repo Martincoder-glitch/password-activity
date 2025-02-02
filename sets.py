11015892a9726fde51bc0d89df47520d0c48899a
Martincoder-glitch <Martinsasuke20241@outlook.com>
2025-02-02 17:56:38 +0000

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,15 @@
+my_set={1,2,3,4,5}
+my_set.add(6)
+my_set.remove(2)
+print(my_set)
+#Set Operations
+# 1) Union
+# 2) Intersection
+# 3) Difference
+# 4) Symmetric Difference
+set1={1,2,3,4,5}
+set2={3,4,5,7,8}
+print (set1|set2)
+print(set1&set2)
+print(set2-set1)
+print(set1^set2)
---
+++ tuples.py
@@ -0,0 +1,7 @@
+my_tuple=("cap","ball","phone")
+print(my_tuple[2])
+print(my_tuple[0:2])
+print(len(my_tuple))
+item,item2,item3 =my_tuple
+print(item)
+#my_tuple[1]="sport"
