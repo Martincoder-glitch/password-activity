dd3a5d286516ac493a5ca872d6d3b1cb228d707a
Martincoder-glitch <Martinsasuke20241@outlook.com>
2025-02-23 17:18:26 +0000

    Add files via upload

--- space game.py
+++ space game.py
@@ -7,6 +7,7 @@ rocket.y=450
 bullets=[]
 aliens=[]
 score=0
+health=100
 for x in range(5):
     for y in range (5):
         alien=Actor("alien")
@@ -19,13 +20,19 @@ def draw():
     screen.clear()
     screen.blit("gmgfhag",(0,0))
     screen.draw.text("Score :"+str(score),(20,20),color="orange")
+    screen.draw.text("health :"+str(health),(40,40),color="orange")
+    if score==25:
+        screen.draw.text("you have won:",(400,300))
+    if health<=0:
+        screen.draw.text("you have lost:",(500,300))
     rocket.draw()
     for bullet in bullets:
-        bullet.draw()
+        bullet.draw()  
     for alien in aliens:
         alien.draw()
 def update():
     global score
+    global health
     if (keyboard.left):
         rocket.x -=5
     if (keyboard.right):
@@ -38,7 +45,10 @@ def update():
             if alien.colliderect(bullet):
                 score=score+1
                 aliens.remove(alien)
-                bullets.remove(bullet)
+                bullets.remove(bullet) 
+            if alien.colliderect(rocket):
+                health=health-1
+            
 def on_key_down(key):
     if key==keys.SPACE:
         bullet=Actor("bullet")
